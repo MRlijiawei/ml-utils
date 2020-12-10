860419ae61fbab8f78dcf66eb268608187521444
李嘉威 <1341924634@qq.com>
2020-12-10 11:05:16 +0800

    TODO——找回丢失文件

---
+++ 别的丢失了？.js
@@ -0,0 +1,8 @@
+// 手动触发resize
+        //   if(document.createEvent) {
+        //     var event = document.createEvent("HTMLEvents");
+        //     event.initEvent("resize", true, true);
+        //     window.dispatchEvent(event);
+        // } else if(document.createEventObject) {
+        //     window.fireEvent("onresize");
+        // }
